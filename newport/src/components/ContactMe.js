cab4da8c40b4e113f87d3528fe33621fd335ed97
Prosper Oshokhenoya Agwegiokhe <prosper.agwegiokhe@CBUBRC02QD99SG8WP.local>
2023-07-04 20:32:46 -0300

    newport

--- newport/src/components/ContactMe.js
+++ newport/src/components/ContactMe.js
@@ -1,14 +1,81 @@
 import styled from "./ContactMe.module.css"
-import { Reveal } from "./Reveal"
-
+import { FadeIn, Reveal, SlideLeft, SlideRight } from "./Reveal"
+import ForumRoundedIcon from '@mui/icons-material/ForumRounded';
+import AddLocationAltRoundedIcon from '@mui/icons-material/AddLocationAltRounded';
+import MarkEmailReadRoundedIcon from '@mui/icons-material/MarkEmailReadRounded';
+import PhoneInTalkRoundedIcon from '@mui/icons-material/PhoneInTalkRounded';
 
 export const ContactMe = () => {
   return (
     <div id="contact" className={styled.contact}>
       <div className={styled.container}>
-        <Reveal delay={0.2}>
+        <FadeIn delay={0.2}>
           <h1>ContactMe<span>.</span> <hr /></h1>
-        </Reveal>
+        </FadeIn>
+
+        <div className={styled.section}>
+          <div className={styled.aside}>
+            <Reveal delay={0.3}>
+              <h2>
+                Let's talk
+                <ForumRoundedIcon
+                  className={styled.icon_large}
+                />
+              </h2>
+            </Reveal>
+           <Reveal delay={0.4}>
+              <form >
+                <div className={styled.email}>
+                  <input
+                    type="email"
+                    required
+                  />
+                  <label>Enter email address</label>
+                </div>
+                <button>Contact me Prosper</button>
+              </form>
+           </Reveal>
+          </div>
+
+          <div className={styled.footer} >
+            <SlideLeft delay={0.5}>
+              <div className={styled.card}>
+                <a
+                  href="https://bit.ly/3JJ6IXO" target="blank">
+                  <AddLocationAltRoundedIcon />
+                </a>
+                <h4>Location</h4>
+                <hr />
+                <p>Sao Paulo, Brasil</p>
+              </div>
+            </SlideLeft>
+            <Reveal delay={0.7}>
+              <div className={styled.card}>
+                <MarkEmailReadRoundedIcon />
+                <h4>Email</h4>
+                <hr />
+                <a href="mailto:piinoya@gmail.com" target="blank">
+                  <p>piinoya@gmail.com</p>
+                </a>
+              </div>
+            </Reveal>
+            <SlideRight delay={0.5}>
+              <div className={styled.card}>
+                <PhoneInTalkRoundedIcon />
+                <h4>Phone</h4>
+                <hr />
+                <a href="tel:" target="blank">
+                  <p>+55 (11) 95141 - 3228</p>
+                </a>
+              </div>
+            </SlideRight>
+          </div>
+          <FadeIn delay={0.8}>
+            <p className={styled.c}>
+              Phingaz © 2023
+            </p>
+          </FadeIn>
+        </div>
       </div>
     </div>
   )
